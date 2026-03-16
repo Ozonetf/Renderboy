9bad565ca749b40a6eaa16d0975b0ebd0eef92b1
owent <owentsao011214@gmail.com>
2026-03-16 13:46:47 +1100

    Restructured glad as library

--- main.cpp
+++ main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
-#include "include/glad/glad.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }    
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "MyOpenGLProject", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+        glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
