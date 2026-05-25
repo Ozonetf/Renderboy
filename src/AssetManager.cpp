ada5c3ec04ef674e8a6f13d1cf28f2855b54e0ed
Owen Tsao <owentsao011214@gmail.com>
2026-05-25 23:04:26 +1000

    Fixing windows w_char filename compatability

--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -29,7 +29,10 @@ void AssetManager::loadTextures()
         auto filename = texFile.path().filename();
         std::cerr << std::format("loading {}\n", filename.string());
         auto iter = m_textures.try_emplace(filename.stem().string());
-        auto succ = iter.first->second.createFromFile(texFile.path().c_str(), true);
+        // Create a generic string and conver to char, otherwise on windows c_str returns
+        // w_char. need to create a copy so creatFromFile has a valid char pointer.
+        auto pathString = texFile.path().string();
+        auto succ = iter.first->second.createFromFile(pathString.c_str(), true);
         // TODO:: better impl? currently creating a tex no matter if file is valid/exsist or not
         if (!succ)
             m_textures.erase(iter.first);
@@ -58,7 +61,8 @@ void AssetManager::loadMeshes()
         std::cerr << std::format("loading {}\n", fbxFile.path().string());
         auto mesh = &m_meshes.try_emplace(fbxFile.path().filename().stem().string(), Mesh{}).first->second;
         mesh->init();
-        mesh->loadModelFromFile(fbxFile.path().c_str());
+        auto pathString = fbxFile.path().string();
+        mesh->loadModelFromFile(pathString.c_str());
     }
 }
 
