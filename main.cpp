4da393cb5b1760a29d831b5703e6024b038eb5d9
Owen Tsao <owentsao011214@gmail.com>
2026-06-12 01:11:01 +1000

    error callback function for OpenGL debug

--- main.cpp
+++ main.cpp
@@ -17,6 +17,68 @@ void winResizeCallback(GLFWwindow *window, int width, int height)
     std::cerr << std::format("x: {} y: {}\n", width, height);
 }
 
+// GL error callback, credit: https://github.com/fendevel/Guide-to-Modern-OpenGL-Functions
+void GLMsgCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const *message,
+                   void const *user_param)
+{
+    auto const src_str = [source]() {
+        switch (source)
+        {
+        default:
+        case GL_DEBUG_SOURCE_API:
+            return "API";
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            return "WINDOW SYSTEM";
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            return "SHADER COMPILER";
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            return "THIRD PARTY";
+        case GL_DEBUG_SOURCE_APPLICATION:
+            return "APPLICATION";
+        case GL_DEBUG_SOURCE_OTHER:
+            return "OTHER";
+        }
+    }();
+
+    auto const type_str = [type]() {
+        switch (type)
+        {
+        default:
+        case GL_DEBUG_TYPE_ERROR:
+            return "ERROR";
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            return "DEPRECATED_BEHAVIOR";
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            return "UNDEFINED_BEHAVIOR";
+        case GL_DEBUG_TYPE_PORTABILITY:
+            return "PORTABILITY";
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            return "PERFORMANCE";
+        case GL_DEBUG_TYPE_MARKER:
+            return "MARKER";
+        case GL_DEBUG_TYPE_OTHER:
+            return "OTHER";
+        }
+    }();
+
+    auto const severity_str = [severity]() {
+        switch (severity)
+        {
+        default:
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            return "NOTIFICATION";
+        case GL_DEBUG_SEVERITY_LOW:
+            return "LOW";
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            return "MEDIUM";
+        case GL_DEBUG_SEVERITY_HIGH:
+            return "HIGH";
+        }
+    }();
+    std::cerr << "\033[31m" << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message
+              << "\033[0m\n";
+}
+
 int main(void)
 {
     // Initialize GLFW
@@ -53,6 +115,11 @@ int main(void)
         std::cerr << "Failed to initialize GLAD\n";
         return -1;
     }
+    glEnable(GL_DEBUG_OUTPUT);
+    // generally dont care about notifications for error callback
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
+    glDebugMessageCallback(GLMsgCallback, nullptr);
+
     // Create and initialize game
     auto &game = Game::instance();
     if (const auto success = game.init(_window, _width, _height); success == -1)
